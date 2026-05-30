6c207b001d086cd08c2dc6120d8a8e02eb643815
Eugene Aidoo <2425403382@live.gctu.edu.gh>
2026-05-30 11:35:30 -0700

    Assignment done

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Eugene Aidoo
+# STUDENT ID:  2425403382
+# Email: Henrygranville83@gmail
+# Grader: Augustus
+#
+# On my honor, Eugene Aidoo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials E, A, and B (stand-in) in large stylized block letters
+    # made of their own letter characters and periods. Each letter is 12 wide
+    # by 10 high, separated by 3-period columns with a 4-asterisk period block.
 
+    print()
+    print("...EAB")
+    print()
+    print("...EEEEEEEEEEEE............AAAA............BBBBBBBBBB.........")
+    print("...EEEEEEEEEEEE...........AAAAAA...........BB........BB.......")
+    print("...EE....................AA....AA..........BB........BB.......")
+    print("...EE...................AA......AA.........BB........BB.......")
+    print("...EEEEEEEEEE..........AAAAAAAAAAAA........BBBBBBBBBB.........")
+    print("...EEEEEEEEEE..........AAAAAAAAAAAA........BBBBBBBBBB.........")
+    print("...EE..................AA........AA........BB........BB.......")
+    print("...EE..................AA........AA........BB........BB.......")
+    print("...EEEEEEEEEEEE...**...AA........AA...**...BB........BB.....**")
+    print("...EEEEEEEEEEEE...**...AA........AA...**...BBBBBBBBBB.......**")
+    print()
 
 
 main()
